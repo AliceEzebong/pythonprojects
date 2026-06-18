9597ca1f47d98e085da57110eb1f3b09879365f9
Alice Ezebong <ezebongalice@gmail.com>
2026-06-17 23:39:28 -0700

    Keep only mini calculator

--- Ministorybuilder.py
+++
@@ -1,6 +0,0 @@
-name = 'Divine'
-place = 'Abuja'
-goal = 'To become a dancer'
-
-print(name, 'lives in', place)
-print('Her goal is', goal)
--- joke generator.py
+++
@@ -1,5 +0,0 @@
-joke_1 = 'Why do programmers hate nature?'
-joke_2 = 'Because it has too many bugs!'
-
-print(joke_1)
-print(joke_2)
